edced2c4162bc03098cb687d91cf2065f4592adc
ArtemG1111 <artem.grishnev03@gmail.com>
2023-12-25 21:51:19 +0200

    puka

---
+++ ProductCatalog.DataAccess/Data/Models/Configuration/ProductConfiguration.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ProductCatalog.DataAccess.Data.Models.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(i => i.Id);
+        }
+    }
+}
---
+++ ProductCatalog.DataAccess/Data/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.DataAccess.Data.Models;
+using System.Reflection;
+
+namespace ProductCatalog.DataAccess.Data
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
