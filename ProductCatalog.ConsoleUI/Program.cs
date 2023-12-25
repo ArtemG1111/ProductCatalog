18d55c38a5632f21ff26eb5d81846f24e91ca328
ArtemG1111 <artem.grishnev03@gmail.com>
2023-12-25 20:46:03 +0200

    PAN-190:Product Catalog start

---
+++ ProductCatalog.ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ProductCatalog.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = GetConfiguration();
+            var serviceProvider = GetServiceProvider(config);
+
+        }
+        static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettiings.json");
+            return builder.Build();
+        }
+        static IServiceProvider GetServiceProvider(IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DeffaultConnection");
+            IServiceCollection services = new ServiceCollection();
+            return services.BuildServiceProvider();
+        }
+
+    }
+}
+    
